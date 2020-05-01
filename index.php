f2a0d7e947ca5625799346b5da6a136047a7c402
root <root@vps-4336327.saudecorporativahsl.com.br>
2020-05-01 17:32:53 -0300

    iniciamos o projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+ini_set( 'display_errors', 1); 
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM php_tb_usuarios ORDER BY deslogin");
+
+
+echo json_encode($usuarios);
+
+
+?>
